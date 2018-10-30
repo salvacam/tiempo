8117825cd4806b12114f91c46d6556fe73a1c2a5
Salvador <salvacams@gmail.com>
2018-10-30 15:04:40 +0100

    Update main_max.js

--- js/main_max.js
+++ js/main_max.js
@@ -213,7 +213,7 @@ var app = {
 
   formatRain: function(rain){
     if (rain !== "Ip") {
-      rain += " mm";
+        return `<div class='rain'>${rain} <span class="min">mm</span></div>`;
     }
     return `<div class='rain'>${rain}</div>`;
   },
