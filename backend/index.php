4ccfcf90d7d5c3abf7221fe1a05d0d1ef6483273
salvacam <salvacams@gmail.com>
2018-11-04 11:31:50 +0100

    backend listado municipios

--- backend/index.php
+++ backend/index.php
@@ -9,11 +9,48 @@ date_default_timezone_set('Europe/Madrid');
 
 $db = new JsonDB("./db/");
 
-/*
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
+
+
+if (isset($_GET["listado"]) ) {
+		
+	// Devolver lo guardado		
+	$municipiosGuardados = $db->selectAll("municipios");
+	if (count($municipiosGuardados) > 0) {
+		http_response_code(200);
+		echo json_encode($municipiosGuardados[0]);
+		die();
+	}
+
+	//Pedir los datos de municipios
+	$urlOpendata = "https://opendata.aemet.es/opendata/api/maestro/municipios/?api_key=" . $apiKey;
+
+	$result = file_get_contents($urlOpendata);
+	$codificado = utf8_encode($result);	
+	$jsonDatos = json_decode($codificado, true);
+
+	$nombres = array();
+	foreach ($jsonDatos as &$valor) {
+		array_push($nombres,array('nombre'=>$valor['nombre'], 'id'=> $valor['id']));
+	}
+
+	if (count($nombres) === 0){
+		$rtn = array("error" => "No se pueden obtener los datos de los municipios");
+	    http_response_code(200);
+	    print json_encode($rtn);
+		die();
+	}
+
+	// Guardar resultado 
+	$db->insert("municipios", $nombres, true);
+    
+    http_response_code(200);
+    print json_encode($nombres);
+	die();
+}
 
 if (!isset($_GET["id"]) || empty($_GET["id"])) {
 	$rtn = array("error" => "Falta identificador de la ciudad o localidad");
